434d16ff4e1eef17e23ec8a124a0ebfe33b692e6
C-joker <koh.itasan@gmail.com>
2025-12-24 19:08:36 +0900

    fixed:Strengthen measures against abnormal input

--- dc.bash
+++ dc.bash
@@ -2,15 +2,19 @@
 # SPDX- SPDX-FileCopyrightText: 2025 C-joker
 # SPDX-License-Identifier: BSD-3-Clause
 
+# 8桁の数字チェック
 if ! echo "$1" | grep -qE '^[0-9]{8}$'; then
-	echo "please write YYYYMMDD"
-	exit 1
+    echo "please write YYYYMMDD"
+    exit 1
 fi
-target=$(date -d "$1" +$s 2>/dev/null)
-if [[ -z "$target" ]];then
-	echo "please write YYYYMMDD"
-	exit 1
+
+# 日付として有効かチェック
+target=$(date -d "$1" +%s 2>/dev/null)
+if [[ -z "$target" ]]; then
+    echo "please write YYYYMMDD"
+    exit 1
 fi
+
 today=$(date +%s)
 diff_days=$(( (target - today) / 86400 ))
 echo "$diff_days 日後"
