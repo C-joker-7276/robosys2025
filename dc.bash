93ffb858fd94247d8442ba293e8715eba15d7f6b
C-joker <koh.itasan@gmail.com>
2025-12-24 18:10:33 +0900

    fixed:Strengthen measures against abnormal input

--- dc.bash
+++ dc.bash
@@ -2,11 +2,15 @@
 # SPDX- SPDX-FileCopyrightText: 2025 C-joker
 # SPDX-License-Identifier: BSD-3-Clause
 
-if [[ "$1" =~ ^[0-9]{8}$ ]]; then
-    today=$(date +%s)
-    target=$(date -d "$1" +%s)
-    diff_days=$(( (target - today) / 86400 ))
-    echo "$diff_days 日後"
-else
-    echo "please write YYYYMMDD"
+if [[ ! "$1" =~ ^[0-9]{8}$ ]]; then
+	echo "please write YYYYMMDD"
+	exit 1
 fi
+target=$(date -d "$1" +$s 2>/dev/null)
+if [[ -z "$target" ]];then
+	echo "please write YYYYMMDD"
+	exit 1
+fi
+today=$(date +%s)
+diff_days=$(( (target - today) / 86400 ))
+echo "$diff_days 日後"
